00a2671873350763a4aa8600a9aa389d71a2eed7
skypyb <pyb068@gmail.com>
2019-10-22 20:05:06 +0800

    [pure-sky] 归档页面的样式调整,给菜单栏增加了css

--- template/archive-page.php
+++ template/archive-page.php
@@ -8,32 +8,6 @@ get_header();
 <?php while (have_posts()) :
 the_post(); ?>
 
-    <style type="text/css">
-        .article-title {
-            text-align: center;
-        }
-        .al_post_list li a {
-            margin-right: 10px;
-            margin-left: 10px;
-            color: #111;
-            text-decoration: none;
-            transition: color 0.3s;
-        }
-
-        .al_post_list li a:hover{
-            color: #00e0e0;
-        }
-
-        .time_base-l {
-            position: relative;
-            margin-bottom: 15px;
-            margin-left: 30px;
-            list-style-type: none;
-            line-height: 30px;
-            box-sizing: border-box;
-            cursor: pointer;
-        }
-    </style>
 <div class="container">
     <div class="content-widescreen">
         <section class="posts">
